50e6b2d972b92c6127c2d2f73df3190a7746e847
Marc Hartmann <107053427+MarcHartmannDev@users.noreply.github.com>
2022-07-11 12:39:03 +0200

    added first API requests

--- server.js
+++ server.js
@@ -12,4 +12,64 @@ app.listen(port, () => {
   console.log(`CORS-enabled Server started on port ${port}`);
 });
 
-console.log(process.env.SECRET_KEY);
+const startpoint = "https://oauth.reddit.com";
+
+app.get("/access_token", async (req, res, next) => {
+  try {
+    const requestToken = await axios.post(
+      "https://www.reddit.com/api/v1/access_token",
+      "grant_type=client_credentials",
+      {
+        auth: {
+          username: process.env.APP_ID,
+          password: process.env.SECRET_KEY,
+        },
+        "Content-Type": "application/x-www-form-urlencoded",
+      }
+    );
+
+    res.status(200).json(requestToken.data);
+  } catch (error) {
+    res
+      .status(error.response.status)
+      .header(error.response.header)
+      .json(error.response.data);
+  }
+});
+
+app.get("/popular", async (req, res, next) => {
+  try {
+    const popular = await axios.get(
+      `${startpoint}/subreddits/popular?limit=${req.query.limit}`,
+      {
+        headers: {
+          Authorization: `Bearer ${req.query.token}`,
+        },
+      }
+    );
+
+    res.status(200).json(popular.data);
+  } catch (error) {
+    res
+      .status(error.response.status)
+      .header(error.response.header)
+      .json(error.response.data);
+  }
+});
+
+app.get("/hot", async (req, res, next) => {
+  try {
+    const top = await axios.get(`${startpoint}/hot`, {
+      headers: {
+        Authorization: `Bearer ${req.query.token}`,
+      },
+    });
+
+    res.status(200).json(top.data);
+  } catch (error) {
+    res
+      .status(error.response.status)
+      .header(error.response.header)
+      .json(error.response.data);
+  }
+});
